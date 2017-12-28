3f755fd84ba17d3fb0844651b4cd35e4916d6385
zarchik <zarchik@inbox.ru>
2017-12-28 20:45:01 +0300

    zZZ

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-sudo ln -s /home/box/webtech18/web /home/box/web 
+sudo ln -s /home/box/WebTech18/web /home/box/web 
 
 #sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/def*
@@ -10,6 +10,7 @@ sudo  ln -s /home/box/web/test_gunicorn.cfg  /etc/gunicorn.d/test_gunicorn.cfg
 sudo /etc/init.d/nginx restart
 sudo systemctl status nginx.service
 
+#gunicorn -w 4 hello:app
 sudo /etc/init.d/gunicorn restart
 sudo systemctl status gunicorn.service
 

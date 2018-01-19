4a2807ed0bb93566ea0fc81f4582410927583e3d
zarchik <zarchik@inbox.ru>
2018-01-19 19:30:49 +0300

    4444

--- web/init.sh
+++ web/init.sh
@@ -2,14 +2,14 @@ sudo ln -s /home/box/WebTech18/web /home/box/web
 
 #sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/def*
-sudo  ln -s /home/box/web/test_nginxDjango.cfg   /etc/nginx/sites-enabled/test.conf
+sudo  ln -sf /home/box/web/test_nginxDjango.cfg   /etc/nginx/sites-enabled/test.conf
 
 
 #test_gunicornDjango.cfg 
 #test_nginxDjango.cfg 
 
-sudo  ln -s /home/box/web/test_gunicorn.cfg   /etc/gunicorn.d/test_gunicorn.cfg
-sudo  ln -s /home/box/web/test_gunicornDjango.cfg   /etc/gunicorn.d/test_gunicornDjango.cfg
+sudo  ln -sf /home/box/web/test_gunicorn.cfg   /etc/gunicorn.d/test_gunicorn.cfg
+sudo  ln -sf /home/box/web/test_gunicornDjango.cfg   /etc/gunicorn.d/test_gunicornDjango.cfg
 
 
 sudo /etc/init.d/nginx restart

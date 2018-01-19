2ef2ed0d3649521f07fb87d13f7284b3b44cbba5
zarchik <zarchik@inbox.ru>
2018-01-19 17:24:44 +0300

    new task Django

---
+++ init18.sh
@@ -0,0 +1,20 @@
+sudo ln -s /home/box/WebTech18/web /home/box/web 
+
+#sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo rm /etc/nginx/sites-enabled/def*
+sudo  ln -s /home/box/web/test.cfg  /etc/nginx/sites-enabled/test.conf
+
+sudo  ln -s /home/box/web/test_gunicorn.cfg  /etc/gunicorn.d/test_gunicorn.cfg
+
+
+sudo /etc/init.d/nginx restart
+sudo systemctl status nginx.service
+
+#gunicorn -w 4 hello:app
+sudo /etc/init.d/gunicorn restart
+sudo systemctl status gunicorn.service
+
+
+#sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+#sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/mysql start

d39f0b8769876a960b4c3ab8ab84f7ee2fcc7d0f
zarchik <zarchik@inbox.ru>
2018-01-19 19:57:30 +0300

    absolute path returned

--- web/init.sh
+++ web/init.sh
@@ -2,14 +2,14 @@
 
 #sudo ln -s  etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/def*
-sudo  ln -sf  test_nginxDjango.cfg   /etc/nginx/sites-enabled/test.conf
+sudo  ln -sf  /home/box/web/test_nginxDjango.cfg   /etc/nginx/sites-enabled/test.conf
 
 
 #test_gunicornDjango.cfg 
 #test_nginxDjango.cfg 
 
-sudo  ln -sf  test_gunicorn.cfg   /etc/gunicorn.d/test_gunicorn.cfg
-sudo  ln -sf  test_gunicornDjango.cfg   /etc/gunicorn.d/test_gunicornDjango.cfg
+sudo  ln -sf  /home/box/web/test_gunicorn.cfg   /etc/gunicorn.d/test_gunicorn.cfg
+sudo  ln -sf  /home/box/web/test_gunicornDjango.cfg   /etc/gunicorn.d/test_gunicornDjango.cfg
 
 
 sudo /etc/init.d/nginx restart

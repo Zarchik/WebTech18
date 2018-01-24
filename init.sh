fd417accc993bc33a1d2d04038bf19a788da672e
zarchik <zarchik@inbox.ru>
2018-01-24 17:23:44 +0300

    22

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
-#sudo ln -s /home/box/WebTech18/web /home/box/web 
+sudo ln -s /home/box/WebTech18/web /home/box/web 
 
 #sudo ln -s  etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm /etc/nginx/sites-enabled/def*
@@ -13,11 +13,11 @@ sudo  ln -f  test_gunicornDjango.cfg   /etc/gunicorn.d/test_gunicornDjango.cfg
 
 
 sudo /etc/init.d/nginx restart
-sudo systemctl status nginx.service
+#sudo systemctl status nginx.service
 
 #gunicorn -w 4 hello:app
 sudo /etc/init.d/gunicorn restart
-sudo systemctl status gunicorn.service
+#sudo systemctl status gunicorn.service
 
 ### TEST w/o service
 # gunicorn --bind 0.0.0.0:8000 ask.wsgi
